7e108b9df8cbba355d91d07015a7499e89c75044
DiddyHop <66392410+DiddyHop@users.noreply.github.com>
2025-02-25 15:11:18 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    // Simple Brief --
+    // Write a function to calculate the score for a patient. This is a simple rule-of-thumb score used to identify ill patients.
+    //  ---
+    // External classes:
+    // - Patient
+    // -- Contains respirationRate, o2Saturation, level of consciousness or new confusion, and temperature.
+    //
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
