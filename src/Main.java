49ae1ccf05a2e1d160e11fcba442d8022f166ab4
DiddyHop <66392410+KMREEVE@users.noreply.github.com>
2025-02-26 15:10:46 +0000

    Final

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,51 @@ public class Main {
     // External classes:
     // - Patient
     // -- Contains respirationRate, o2Saturation, level of consciousness or new confusion, and temperature.
-    //
+    // ####
+    // This class drives the program by creating 3 objects representing different patients and printing their mediscore
+    // TO-DO
+    // - BONUS TASK 1 -
+    // Alerting for trends in the Medi score -
+    // While the score is useful on its own to assess the urgency of treatment,
+    // an increasing score over a short period of time would be worth notifying someone about.
+    // Can your system flag up an additional risk if a score has raised by more than 2 points within a 24 hour period?
+    // - BONUS TASK 2 -
+    //Capillary Blood Glucose is another metric that is regularly recorded, but its range changes depending on when the patient last ate.
+    // The ranges (in mmol/L) and scores are as follows:
+    // (Check table: https://github.com/airelogic/tech-test-portal/tree/main/Medi-Score-Calculation)
+
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // If this were real, the values would be inputted by the user with the timestamp automatically gathered from the system.
+        // Patient 1 from Examples
+        Patient patient1 = new Patient("Patient 1", Patient.AirType.AIR, Patient.ConsciousnessLevel.ALERT, 15, 95, 37.1f, Patient.LastMeal.WITHIN_2HRS, 5.9f);
+
+        // Patient 2 from Examples
+        Patient patient2 = new Patient("Patient 2", Patient.AirType.OXYGEN, Patient.ConsciousnessLevel.ALERT, 17, 95, 37.1f,Patient.LastMeal.FASTING, 3.6f);
+
+        // Patient 3 from Examples
+        Patient patient3 = new Patient("Patient 3", Patient.AirType.OXYGEN, Patient.ConsciousnessLevel.CVPU, 23, 88, 38.5f, Patient.LastMeal.FASTING, 6.0f);
+
+        System.out.println("Medi Score of Patient 1 is: " + patient1.CalculateMediScore());
+        patient1.AddMediScore();
+        System.out.println("\nMedi Score of Patient 2 is: " + patient2.CalculateMediScore());
+        patient1.AddMediScore();
+        System.out.println("\nMedi Score of Patient 3 is: " + patient3.CalculateMediScore());
+        patient3.AddMediScore();
+
+        // Debugging for 24 hour alert
+        try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); } // Simulate delay
+
+        patient1.SpO2 = 83;
+        System.out.println("Medi Score of Patient 1 is: " + patient1.CalculateMediScore());
+        patient1.AddMediScore();
+
+        System.out.println("\nMedi Score of Patient 2 is: " + patient2.CalculateMediScore());
+        patient2.AddMediScore();
+
+
+        patient3.SpO2 = 96;
+        System.out.println("\nMedi Score of Patient 3 is: " + patient3.CalculateMediScore());
+        patient3.AddMediScore();
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,2 +1,170 @@
-﻿public class Patient {
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+public class Patient {
+
+    // Required:
+    // - Air or Oxygen
+    // | Enum(Integer) | 0 if air, 2 if on oxygen
+    // ####
+    // - Level of consciousness or new confusion (whether the patient is newly confused, disorientated or agitated)
+    // | Enum(Integer) | 0 if alert, non-zero if CVPU
+    // ####
+    // - Respiration Rate
+    // | Integer
+    // ####
+    // - Oxygen Saturation (SpO2)
+    // | Integer
+    // ####
+    // - Temperature
+    // | Float | This should be rounded to a single decimal place.
+
+    // -- Declarations --
+    public String myName;
+    enum AirType {                  // Air or Oxygen
+        AIR(0), OXYGEN(2);
+        private final int value;
+        AirType(int value) { this.value = value; }
+        public int getValue() { return value; }
+    }
+    public AirType currentAirType;
+
+    enum ConsciousnessLevel {        // Level of Consciousness
+        ALERT(0), CVPU(3);
+        private final int value;
+        ConsciousnessLevel(int value) { this.value = value; }
+        public int getValue() { return value; }
+    }
+    public ConsciousnessLevel currentConsciousnessLevel;
+
+    enum LastMeal {     // For CBG
+        WITHIN_2HRS,
+        FASTING,
+    }
+    public LastMeal lastMeal;
+
+    public Integer respRate;               // Respiration Rate
+    public Integer SpO2;                   // Oxygen Saturation
+    public Float temperature;              // Temperature
+    public Float cbgLevel;                 // Capillary Blood Glucose
+
+    // -- Constructor --
+    public Patient(String myName, AirType myAirType, ConsciousnessLevel myConsciousnessLevel, Integer myRespRate, Integer mySpO2, Float myTemperature, LastMeal myLastMeal, Float myCbgLevel) {
+        this.myName = myName;
+        this.currentAirType = myAirType;
+        this.currentConsciousnessLevel = myConsciousnessLevel;
+        this.respRate = myRespRate;
+        this.SpO2 = mySpO2;
+        this.temperature = Math.round(myTemperature * 10.0f) / 10.0f;
+        this.lastMeal = myLastMeal;
+        this.cbgLevel = myCbgLevel;
+    }
+
+    // Time Stamped Score Entry
+    private static class TStampedMediScore {
+        int score;
+        LocalDateTime timeStamp;
+
+        TStampedMediScore(int score, LocalDateTime timeStamp){
+            this.score = score;
+            this.timeStamp = timeStamp;
+        }
+    }
+
+    private LinkedList<TStampedMediScore> mediScoreHistory = new LinkedList<>();
+
+    public void AddMediScore() {
+        Integer newScore = CalculateMediScore();
+        LocalDateTime now = LocalDateTime.now();
+
+        // Removes Medi Scores older than 24 hours
+        mediScoreHistory.removeIf(entry -> entry.timeStamp.isBefore(now.minusHours(24)));
+
+        // Alerts if score has increased by more than two points
+        if (!mediScoreHistory.isEmpty() && (newScore - mediScoreHistory.getFirst().score) > 2) {
+            System.out.println("ALERT !! Medi Score for " + myName + " has increased by more than 2 points in 24 hours!!!");
+        }
+
+        // Adds the new score
+        mediScoreHistory.add(new TStampedMediScore(newScore, now));
+    }
+
+    // -- Main --
+    // Calculate Medi Score
+    public Integer CalculateMediScore(){
+        System.out.println(GetAirScore(currentAirType));
+        System.out.println(GetConciousnessScore(currentConsciousnessLevel));
+        System.out.println(GetRespScore(respRate));
+        System.out.println(GetSpO2Score(SpO2, currentAirType));
+        System.out.println(GetTempScore(temperature));
+        System.out.println(GetCBGScore(cbgLevel, lastMeal)); // New CBG score calculation
+        Integer mediScore =
+                GetAirScore(currentAirType)
+                        + GetConciousnessScore(currentConsciousnessLevel)
+                        + GetRespScore(respRate)
+                        + GetSpO2Score(SpO2, currentAirType)
+                        + GetTempScore(temperature)
+                        + GetCBGScore(cbgLevel, lastMeal);
+        return mediScore;
+    }
+
+    // -- Getting of score functions --
+    // Get Air Score
+    public static Integer GetAirScore(AirType currentAirType){
+        return currentAirType.getValue();
+    }
+
+    // Get Conciousness Score
+    public static Integer GetConciousnessScore(ConsciousnessLevel currentConciousnessLevel){
+        return currentConciousnessLevel.getValue();
+    }
+
+    // Get Respiration Rate Score
+    public static Integer GetRespScore(Integer currentRespRate){
+        if (currentRespRate <= 8 || currentRespRate >= 25) {
+            return 3;
+        } else if (currentRespRate >= 9 && currentRespRate <= 11) {
+            return 1;
+        } else if (currentRespRate >= 12 && currentRespRate <= 20) {
+            return 0;
+        } else if (currentRespRate >= 21 && currentRespRate <= 24) {
+            return 2;
+        }
+        return 0;
+    }
+
+    // Get Oxygen Saturation Score
+    public static Integer GetSpO2Score(Integer currentSpO2, AirType currentAirType){
+        if (currentSpO2 <= 83) return 3;
+        if (currentSpO2 >= 97 && currentAirType == AirType.OXYGEN) return 3;
+        if ((currentSpO2 == 84 || currentSpO2 == 85) || ((currentSpO2 == 95 || currentSpO2 == 96) && currentAirType == AirType.OXYGEN)) return 2;
+        if ((currentSpO2 == 86 || currentSpO2 == 87) || ((currentSpO2 == 93 || currentSpO2 == 94) && currentAirType == AirType.OXYGEN)) return 1;
+        if ((currentSpO2 >= 88 && currentSpO2 <= 92) || (currentSpO2 >= 93 && currentAirType == AirType.AIR)) return 0;
+        return 0;
+    }
+
+    // Get Temperature Score
+    public static Integer GetTempScore(Float currentTemp){
+        if (currentTemp < 35.0f) return 3;
+        if (currentTemp > 39.1f) return 2;
+        if (currentTemp >= 38.1f && currentTemp <= 39.0f) return 1;
+        if (currentTemp >= 36.1f && currentTemp <= 38.0f) return 0;
+        return 0;
+    }
+
+    // Get CBG Score
+    public static Integer GetCBGScore(Float cbgLevel, LastMeal lastMeal){
+        if (lastMeal == LastMeal.FASTING){
+            if (cbgLevel <= 3.4f || cbgLevel >= 6.0f) return 3;
+            if (cbgLevel >= 5.5f && cbgLevel <= 5.9f) return 2;
+            if (cbgLevel >= 3.5f && cbgLevel <= 3.9f) return 2;
+            if (cbgLevel >= 4.0f && cbgLevel <= 5.4f) return 0;
+        }else {     // Has eaten within 2 hours
+            if (cbgLevel <= 4.5f || cbgLevel >= 9.0f) return 3;
+            if (cbgLevel >= 7.9f && cbgLevel <= 8.9f) return 2;
+            if (cbgLevel >= 4.5f && cbgLevel <= 5.8f) return 2;
+            if (cbgLevel >= 5.9f && cbgLevel <= 7.8f) return 0;
+        }
+        return 0;
+    }
 }
